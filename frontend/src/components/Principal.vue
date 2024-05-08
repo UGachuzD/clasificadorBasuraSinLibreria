2bd4b8b94497bba2cdf9e4ba6c5e8d3616d65df2
UGachuzD <ulisesgachuz07@gmail.com>
2024-05-07 21:29:39 -0600

    Merge branch 'master' of

--- frontend/src/components/Principal.vue
+++ frontend/src/components/Principal.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <v-container class="fill-height mx-auto">
     <v-responsive
       class="align-center fill-height mx-auto text-center"
@@ -54,35 +53,6 @@
           </v-card>
         </v-col>
       </v-row>
-=======
-  <v-container class="fill-height">
-    <v-responsive class="align-center fill-height mx-auto" max-width="1000">
-      <v-card color="white" class="elevation-12" max-width="1000" height="500" outlined>
-        <v-card-title class="text-center">CLASIFICADOR DE RESIDUOS</v-card-title>
-        <v-container>
-          <v-row justify="center">
-            <v-col cols="5">
-              <v-card class="pa-6" max-width="600" height="400" outlined>
-                <v-card-title class="text-center">Archivo BMP a clasificar:</v-card-title>
-                <v-file-input label="Seleccionar" variant="outlined" @change="handleFileUpload"></v-file-input>
-                <!-- Campo para mostrar información -->
-                <v-text-field v-model="clasificacionResiduo" label="Clasificación de residuo" readonly></v-text-field>
-                <!-- Botón para iniciar la clasificación -->
-                <v-btn color="primary" @click="clasificarResiduo" :disabled="!selectedImage">Clasificar</v-btn>
-                <!-- Botón para limpiar campos -->
-                <v-btn color="error" @click="limpiarCampos">Limpiar</v-btn>
-              </v-card>
-            </v-col>
-            <v-col cols="7">
-              <v-card class="pa-6" max-width="600" height="400" outlined>
-                <v-card-title class="text-center">Visualización de la imagen:</v-card-title>
-                <v-img :src="selectedImage" max-width="100%" max-height="90%"></v-img>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
->>>>>>> cff0f5d41582757e243619c4880b8d013f69a06e
     </v-responsive>
   </v-container>
 </template>
