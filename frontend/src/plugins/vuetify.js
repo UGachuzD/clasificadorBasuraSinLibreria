a5624cd312eb486dfec05419db59c77d281ef01d
UGachuzD <ulisesgachuz07@gmail.com>
2024-05-07 21:08:44 -0600

    Cambio tema

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -14,6 +14,12 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    claro: true,
+    themes: {
+      claro: {
+        primary: "#19647E",
+        secondary: "#424242",
+      },
+    },
   },
-})
+});
